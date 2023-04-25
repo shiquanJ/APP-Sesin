23190c15a04ef5e0b062405732d9f2f707d13779
shiquanJ <jiangshiquan2020@163.com>
2023-04-25 17:01:56 +0800

    안드로이드 apk/aab 버전 업데이트
    물리적 back 탭 클릭 이벤트 추가

--- App.js
+++ App.js
@@ -1,18 +1,71 @@
-import * as React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { WebView } from 'react-native-webview';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet,BackHandler,ToastAndroid  } from 'react-native';
 import Constants from 'expo-constants';
 import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+export default () => {
+  const webview = useRef();
+  let exitApp = false;
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+  const onPressHardwareBackButton = () => {
+    if (webview.current && isCanGoBack) {
+      webview.current.goBack();
+      return true;
+    } else {
+      if (exitApp == undefined || !exitApp) {
+        exitApp = true
+        ToastAndroid.show("한번 더 뒤로가기 입력시 종료됩니다.",ToastAndroid.SHORT)
+        timeOut = setTimeout(() => {
+          exitApp = false
+        }, 2000)
+      } else {
+        clearTimeout(timeOut)
+        BackHandler.exitApp()// 앱종료 
+      }
+      return true
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onPressHardwareBackButton);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onPressHardwareBackButton);
+    }
+  }, [isCanGoBack]);
   return (
     <View
       style={styles.container}
     >
       <StatusBar style="auto" />
       <WebView
+        ref={webview}
         source={{ uri: 'http://bestsesin.com' }}
-      />
+        injectedJavaScript={`
+          (function() {
+            function wrap(fn) {
+              return function wrapper() {
+                var res = fn.apply(this, arguments);
+                window.ReactNativeWebView.postMessage('navigationStateChange');
+                return res;
+              }
+            }
+      
+            history.pushState = wrap(history.pushState);
+            history.replaceState = wrap(history.replaceState);
+            window.addEventListener('popstate', function() {
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+            });
+          })();
+      
+          true;
+        `}
+      onMessage={({ nativeEvent: state }) => {
+        if (state.data === 'navigationStateChange') {
+          // Navigation state updated, can check state.canGoBack, etc.
+          setIsCanGoBack(state.canGoBack);
+        }
+      }}/>
     </View>
   );
 }
